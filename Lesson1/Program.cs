1cca25c021e67d1690c17d84f5e8f9803f083d74
Evgeniya Bogatova <evgesha-bogatova@mail.ru>
2022-10-08 16:37:40 +0300

    Exersise two is complete

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,15 +1,39 @@
 ﻿//Задание 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
+// Console.Write("Input first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 != num2)
+//     if (num1 > num2)
+//         Console.WriteLine("max number is " + num1);
+//     else
+//         Console.WriteLine("max number is " + num2);
+
+// else
+//     Console.WriteLine("These numbers are equal");
+
+//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
 Console.Write("Input first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input threed number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 != num2)
-    if (num1 > num2)
-        Console.WriteLine("max number is " + num1);
+if (num1 > num2)
+{    
+    if (num1 > num3)
+    Console.WriteLine("Max number is " + num1);
     else
-        Console.WriteLine("max number is " + num2);
-
+    Console.WriteLine("Max number is " + num3);
+}
 else
-    Console.WriteLine("These numbers are equal");
+{
+    if (num2 > num3)
+    Console.WriteLine("Max number is " + num2);
+    else
+    Console.WriteLine("Max number is " + num3);
+}
