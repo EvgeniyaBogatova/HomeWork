62afea7bbd3d703faf1da98ab0104b0fd5983155
Evgeniya Bogatova <evgesha-bogatova@mail.ru>
2022-10-08 16:47:53 +0300

    Exersise three is complete

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -14,26 +14,36 @@
 // else
 //     Console.WriteLine("These numbers are equal");
 
-//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.Write("Input first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input threed number: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input threed number: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2)
-{    
-    if (num1 > num3)
-    Console.WriteLine("Max number is " + num1);
-    else
-    Console.WriteLine("Max number is " + num3);
-}
+// if (num1 > num2)
+// {    
+//     if (num1 > num3)
+//     Console.WriteLine("Max number is " + num1);
+//     else
+//     Console.WriteLine("Max number is " + num3);
+// }
+// else
+// {
+//     if (num2 > num3)
+//     Console.WriteLine("Max number is " + num2);
+//     else
+//     Console.WriteLine("Max number is " + num3);
+// }
+
+//Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+    Console.WriteLine("This number is even");
 else
-{
-    if (num2 > num3)
-    Console.WriteLine("Max number is " + num2);
-    else
-    Console.WriteLine("Max number is " + num3);
-}
+    Console.WriteLine("Thris number is an odd");
