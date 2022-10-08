534c87e13f2f0e250510e430965b26234b94fb45
Evgeniya Bogatova <evgesha-bogatova@mail.ru>
2022-10-08 16:21:24 +0300

    Exersise one is complete

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задание 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 != num2)
+    if (num1 > num2)
+        Console.WriteLine("max number is " + num1);
+    else
+        Console.WriteLine("max number is " + num2);
+
+else
+    Console.WriteLine("These numbers are equal");
