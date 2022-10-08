2cddfe8b128cb5639bb46ddda52cdac1f48ba9e9
Evgeniya Bogatova <evgesha-bogatova@mail.ru>
2022-10-08 23:53:18 +0300

    Add folder Seminar2 and Exercise 1 is done

---
+++ Seminar2/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int RemoveSecond(int num)
+{
+     int sec = num / 10 % 10;
+ return sec;
+ }
+
+Console.Write("Input three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= -100 && number > -1000)
+{
+    number = number*(-1);
+    int des = RemoveSecond(number);
+    Console.WriteLine($"Second digit of -{number} is {des}");
+}
+else
+    if (number >= 100 && number < 1000)
+    {
+        int des = RemoveSecond(number);
+        Console.WriteLine($"Second digit of {number} is {des}");
+    }
+    else
+        Console.WriteLine("Uncorrect imput");
+
+
+
+//Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
