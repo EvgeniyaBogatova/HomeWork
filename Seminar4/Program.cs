e56d0f8de8904a9ca50cbc4d034de1459fb7362e
Evgeniya Bogatova <evgesha-bogatova@mail.ru>
2022-10-12 23:54:56 +0300

    Third exercise done

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -21,46 +21,44 @@
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-int GetSum(int a)
-{
-int res = 0;
-while (a > 0)
-{ 
-    res = res + a % 10;
-    a = a / 10;
-}
-return res;
-}
-Console.Write("Input integer number: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// int GetSum(int a)
+// {
+// int res = 0;
+// while (a > 0)
+// { 
+//     res = res + a % 10;
+//     a = a / 10;
+// }
+// return res;
+// }
+// Console.Write("Input integer number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-    Console.WriteLine($"Sum of the digits of {num} is {GetSum(Math.Abs(num))}");
+//     Console.WriteLine($"Sum of the digits of {num} is {GetSum(Math.Abs(num))}");
 
 // Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
 
-// m = 5 -> [1, 2, 5, 7, 19]
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{ 
+int[] array = new int[size];
+for (int i = 0; i < size; i++)
+    array[i] = new Random().Next(minValue, maxValue + 1);
+return array;
+}
 
-// m = 3 -> [6, 1, 33]
-// int[] CreateRandomArray(int size, int minValue, int maxValue)
-// { int[] array=new int[size];
-// for (int i=0; i<size; i++)
-// array[i] = new Random(). New(minValue, maxValue+1);
-// return array;
-// }
+void ShowArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+Console.Write(array[i]+ " ");
+Console.WriteLine();
+}
 
-// void ShouArray(int [] array)
-// {
-// for (int i=0; I<array.lenght; i++)
-// Console.Write(array[i]+ " ");
-// Console.WriteLine();
-// }
+Console.Write("Input length of array: ");
+int lenght=Convert.ToInt32(Console.ReadLine());
+Console.Write("Input minimal of numbers : ");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.Write("Input maximal of numbers: ");
+int num2=Convert.ToInt32(Console.ReadLine());
 
-// Console.Write("Input length of array: ");
-// int lenght=Convert.ToInt32(Console.ReadLine());
-// Console.Write("Input minimal of numbers : ");
-// int num1=Convert.ToInt32(Console.ReadLine());
-// Console.Write("Input maximal of numbers: ");
-// int num2=Convert.ToInt32(Console.ReadLine());
-
-// int[] ArrayNew = CreateRandomArray(lenght, num1, num2);
-// ShowArray;
+int[] ArrayNew = CreateRandomArray(lenght, num1, num2);
+ShowArray(ArrayNew);
