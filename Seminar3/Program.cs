d8f4d980837d794fa836f817b1edbf2721985612
Evgeniya Bogatova <evgesha-bogatova@mail.ru>
2022-10-17 00:17:46 +0300

    two decimal point

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -53,6 +53,7 @@ double Distance(double x1, double y1, double z1, double x2, double y2, double z2
 {
     
     double res = Math.Sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) + (z2 - z1)*(z2 - z1));
+    res = Math.Round(res, 2);
     return res;
 }
     
