34b2ee0bbde1c8cc5ab861b86fa30cdb26dde7cd
Evgeniya Bogatova <evgesha-bogatova@mail.ru>
2022-10-16 15:57:49 +0300

    Exercise 2 of seminar 5 is complete

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -1,11 +1,47 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-int[] CreateRandomArray(int size)
+// int[] CreateRandomArray(int size)
+// { 
+// int[] array = new int[size];
+// for (int i = 0; i < size; i++)
+//     array[i] = new Random().Next(100, 1000);
+// return array;
+// }
+
+// void ShowArray(int[] array)
+// {
+// for (int i = 0; i < array.Length; i++)
+//     Console.Write(array[i]+ " ");
+// Console.WriteLine();
+// }
+
+// int NumberOfEver (int[] array)
+// {
+// int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//         if (array[i] % 2 == 0)
+//         count++;
+// return count;
+// }
+
+// Console.Write("Input length of array: ");
+// int lenght=Convert.ToInt32(Console.ReadLine());
+
+// int[] ArrayNew = CreateRandomArray(lenght);
+// ShowArray(ArrayNew);
+// Console.WriteLine("Number of ever numbers in an array is " + NumberOfEver(ArrayNew));
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateRandomArray(int size, int min, int max)
 { 
 int[] array = new int[size];
 for (int i = 0; i < size; i++)
-    array[i] = new Random().Next(100, 1000);
+    array[i] = new Random().Next(min, max);
 return array;
 }
 
@@ -16,27 +52,25 @@ for (int i = 0; i < array.Length; i++)
 Console.WriteLine();
 }
 
-int NumberOfEver (int[] array)
+int GiveSummEvenPosition (int[] array)
 {
-int count = 0;
-    for (int i = 0; i < array.Length; i++)
-        if (array[i] % 2 == 0)
-        count++;
-return count;
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    sum = sum + array[i];
+return sum;
 }
 
 Console.Write("Input length of array: ");
-int lenght=Convert.ToInt32(Console.ReadLine());
+int length=Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min volue of array: ");
+int minVolue=Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max volue of array: ");
+int maxVolue=Convert.ToInt32(Console.ReadLine());
 
-int[] ArrayNew = CreateRandomArray(lenght);
-ShowArray(ArrayNew);
-Console.WriteLine("Number of ever numbers in an array is " + NumberOfEver(ArrayNew));
+int[] Array02 = CreateRandomArray(length, minVolue, maxVolue);
+ShowArray(Array02);
+Console.WriteLine("Sum digits of even positions in array is " + GiveSummEvenPosition(Array02));
 
-
-
-// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
