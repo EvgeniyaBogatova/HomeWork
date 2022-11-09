ef9ead5106a6d80d1ef4db0806a095ff0bd44202
Evgeniya Bogatova <evgesha-bogatova@mail.ru>
2022-11-10 00:30:46 +0300

    Exercise 47 from seminar 7 is complete

---
+++ Seminar7/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double [,] Create2DArray()
+{
+    Console.WriteLine("Set the number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Set the number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+double [,] array = new double[rows,columns];
+Random rnd = new Random();
+for (int i = 0; i < rows; i++)
+    for (int j = 0; j < columns; j++)
+        // array[i,j] = Math.Round((minValue+ (maxValue-minValue)*new Random().NextDouble()),2);
+        array[i,j] = Math.Round((new Random().Next(minValue,maxValue) + new Random().NextDouble()),2);
+return array;
+}
+
+void Show2DArray (double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+    }
+}
+
+double [,] myArray = Create2DArray();
+Show2DArray (myArray);
+
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> числа с такими индексами в массиве нет
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
