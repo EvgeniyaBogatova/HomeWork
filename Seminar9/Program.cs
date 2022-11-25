18b05070b60272e8359fbc712b0e922d9c690184
Evgeniya Bogatova <evgesha-bogatova@mail.ru>
2022-11-25 15:59:29 +0300

    Exercise 68 complete

--- Seminar9/Program.cs
+++ Seminar9/Program.cs
@@ -2,28 +2,28 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-void ShowNumbers (int num)
-{
-    if (num != 1)
-    {
-        if (num > 1)
-    {       Console.Write (num + " ");
-            ShowNumbers (num - 1);
-    }   
-        if (num < 1)
-    {
-            Console.Write (num + " ");
-            ShowNumbers (num + 1);
-    }
-    }
-    else
-    Console.Write ("1");
-}
+// void ShowNumbers (int num)
+// {
+//     if (num != 1)
+//     {
+//         if (num > 1)
+//     {       Console.Write (num + " ");
+//             ShowNumbers (num - 1);
+//     }   
+//         if (num < 1)
+//     {
+//             Console.Write (num + " ");
+//             ShowNumbers (num + 1);
+//     }
+//     }
+//     else
+//     Console.Write ("1");
+// }
 
-Console.Write("Input number: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-ShowNumbers(num);
+// ShowNumbers(num);
 
 
 
@@ -51,3 +51,25 @@ ShowNumbers(num);
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+int FuncAkkerman (int m, int n)
+{
+    int rez = 0;
+    if (m > 0 && n > 0)
+        rez += FuncAkkerman(m-1, FuncAkkerman(m, n-1));
+    
+    if (m > 0 && n == 0)
+        rez += FuncAkkerman(m - 1, 1);
+       
+    if (m == 0)
+        rez += n + 1;
+
+    return rez;
+    
+}
+
+Console.WriteLine("Input first number for function Akkerman: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number for function Akkerman: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Function Akkerman for {m} and {n} is " + FuncAkkerman(m,n));
