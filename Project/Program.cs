a3b5eed1db570148dadb05f645aad62f674f98c3
Evgeniya Bogatova <evgesha-bogatova@mail.ru>
2022-11-26 22:34:11 +0300

    Method array entry ready

---
+++ Project/Program.cs
@@ -0,0 +1,14 @@
+﻿str [] InputArray ()
+{
+Console.WriteLine("Input length of array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+str[] arrayString = new array[length];
+for (int i = 0; i < length; i++)
+    {
+    Console.WriteLine("Input string: ");
+    arrayString[i] = Console.ReadLine();
+    }
+return arrayString;
+}
+
