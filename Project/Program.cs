fd94599e2b3885407e85b0b8770d6fbe489e0463
Evgeniya Bogatova <evgesha-bogatova@mail.ru>
2022-11-27 17:44:08 +0300

    Program is done, if you inpum array on the code

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,34 +1,53 @@
-﻿str [] InputArray ()
-{
-Console.WriteLine("Input length of array: ");
-int length = Convert.ToInt32(Console.ReadLine());
+﻿// string[] InputArray ()
+// {
+// Console.WriteLine("Input length of array: ");
+// int length = Convert.ToInt32(Console.ReadLine());
 
-str[] arrayString = new array[length];
-for (int i = 0; i < length; i++)
-    {
-    Console.WriteLine("Input string: ");
-    arrayString[i] = Console.ReadLine();
-    }
-return arrayString;
-}
+// string[] arrayString = new string[length];
+// for (int i = 0; i < length; i++)
+//     {
+//     Console.WriteLine("Input string: ");
+//     arrayString[i] = Console.ReadLine();
+//     }
+// return arrayString;
+// }
 
-str [] GetStringThree (str [])
+string[] GetStringThree (string[] array)
 {
-int [] array = new int [str.GetLength];
+int n = array.Length;
+int [] index = new int [n];
 
 int count = 0;
 int j = 0;
-for (int i = 0; i < str.GetLength; i++)
-    if (str[i].GetLength <=3 )
-        {array[j] = i;
+for (int i = 0; i < n; i++)
+    {
+    string s = array[i];
+    int v = s.Length;
+    if (v <=3 )
+        {index[j] = i;
         count ++;
         j++;}
+    }
     
 string [] newArray = new string [count];
 j=0;
 for (int i = 0; i < count; i++)
-    {newArray[i] = str[array[j]];
+    {newArray[i] = array[index[j]];
     j++;}
 return newArray;
 }
+
+void ShowArray (string [] array)
+{
+int n =  array.Length;
+for (int i = 0; i < n; i++)
+    Console.Write(array[i] + "; ");
+}
+
+string [] myArray = {"fyd", "jbdskj djhfbjd dfhb", "fiud", "ufd", "2", "jfdie"};
+ShowArray (myArray);
+Console.WriteLine();
+ShowArray (GetStringThree(myArray));
+
+
 
